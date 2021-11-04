8a7b275ca194ceeda14e18d27c0b04f9238d09d4
AZHAR <Azhar89143@gmail.com>
2021-11-05 02:56:16 +0530

    sell data completed

--- sell.php
+++ sell.php
@@ -1,18 +1,35 @@
 <?php
   include 'register.php';
+
   if (isset($_POST['submit'])){
-   $name=$_POST['name'];
-   $email=$_POST['email'];  
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $phone=$_POST['phone'];
+    $city=$_POST['city'];
+    $year=$_POST['year'];
+    $make=$_POST['make'];
+    $model=$_POST['model'];
+    $kms=$_POST['kms'];
+    $own=$_POST['own'];
+    $price=$_POST['price'];
+    $fuel=$_POST['fuel'];
+    $color=$_POST['color'];
+    $reg=$_POST['registertion'];
+    $ins=$_POST['insurance'];
+    $why=$_POST['why'];
+    $image=$_POST['file'];
 
-    $sqli="INSERT INTO sell(city,name,email)VALUES('$name','$email')";
-    $results=mysqli_query($connection,$sqli);
 
-    if ($results) {
-      echo "<script>alert('insert')</script>";
-    }else{
-      echo "<script>alert('Error')</script>";
-    }
-  }  
+
+  $sqli="INSERT INTO student(name,email,phone,city,year,make,model,kms,own,price,image,type,color,regnum,insurance,why) VALUES('$name','$email','$phone','$city','$year','$make','$model','$kms','$own','$price','$image','$fuel','$color','$reg','$ins','$why')";
+  $results=mysqli_query($connection,$sqli);
+
+  if ($results){
+    echo "<script>alert('insert')</script>";
+  }else{
+    echo "<script>alert('Error')</script>";
+  }
+}   
 ?>
 
 
@@ -20,31 +37,31 @@
 
 <div class="sell">
   <h1>Details</h1>
-  <form method="post" action="" name="sellform">
+  <form method="post" action="" name="sellform" >
     <table>
       <tr>
        <td>Name :</td>
-       <td><input type="text" name="name" ></td>
+       <td><input type="text" name="name"required ></td>
       </tr>
 
       <tr>
        <td>Mobile Number:</td>
-       <td><input type="tel" name="phone" ></td>
+       <td><input type="tel" name="phone" required></td>
       </tr>
 
       <tr>
         <td>Email id:</td>
-        <td><input type="email" name="email" ></td>
+        <td><input type="email" name="email"required ></td>
       </tr>
 
       <tr>
           <td>City</td>
-          <td><input type="text" name="city"></td>
+          <td><input type="text" name="city" required></td>
       </tr>
 
       <tr>
          <td>Mfg Year</td>
-         <td><input type="text" name="year"></td>
+         <td><input type="text" name="year" required></td>
       </tr>
 
       <tr>
@@ -60,32 +77,32 @@
             
       <tr>
          <td>Model</td>
-         <td><input type="text" name="model"></td>
+         <td><input type="text" name="model" required></td>
       </tr>
         
       <tr>
           <td>KMs Driven</td>
-          <td><input type="text" name="kms"></td>
+          <td><input type="text" name="kms" required></td>
       </tr>
 
       <tr>
          <td>Num.of Owners</td>
-         <td><input type="text" name="own"></td>
+         <td><input type="text" name="own" required></td>
       </tr>
 
       <tr>
           <td>Expected Price</td>
-          <td><input type="text" name="Price"></td>
+          <td><input type="text" name="price" required></td>
       </tr>
 
       <tr>
           <td>Upload Image</td>
-          <td><input type="file" name="file"></td>
+          <td><input type="file" name="file" required></td>
       </tr>
 
       <tr>
           <td>Fuel Type</td>
-          <td><select name="fuel">
+          <td><select name="fuel" required>
           <option>select</option>
           <option>Petrol</option>
           <option>Diesel</option>
@@ -96,7 +113,7 @@
       <tr>
        <td>Color</td>
        <td>
-        <select name="color">
+        <select name="color" required>
         <option>select</option>
         <option>Red</option>
         <option>Blue</option>
@@ -106,17 +123,17 @@
 
       <tr>
           <td>Registration Number</td>
-          <td><input type="tel" name="registertion"></td>
+          <td><input type="tel" name="registertion" required></td>
       </tr>
 
       <tr>
           <td>  Insurance Valid Till</td>
-          <td><input type="text" name="insurance" ></td>
+          <td><input type="text" name="insurance" required ></td>
       </tr>
 
       <tr>
           <td>Tell the buyer why you should buy your car</td>
-          <td><textarea name="why"></textarea></td>
+          <td><textarea name="why" required></textarea></td>
       </tr>
       <tr>
         <td><input type="submit" name="submit" value="POST"> </td>
